13ed16809fa3617b71a092651a07432ca4d0e612
kmsayem12 <sayem312518@gmail.com>
2025-05-06 00:06:04 +0600

    add public and private stack

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,7 +1,13 @@
-export type RootStackParamList = {
+export type PublicStackParamList = {
   Splash: undefined;
+  Login: undefined;
+};
+
+export type PrivateStackParamList = {
   Home: undefined;
 };
+
+export type RootStackParamList = PublicStackParamList & PrivateStackParamList;
 
 declare global {
   namespace ReactNavigation {
