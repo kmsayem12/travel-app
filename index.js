77cf4f833cd45f13138a5dc100086838d0a255b3
kmsayem12 <sayem312518@gmail.com>
2025-05-08 23:49:26 +0600

    install gifted-chat

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @format
  */
-
+import 'react-native-reanimated'; // 👈 must be at the top
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
