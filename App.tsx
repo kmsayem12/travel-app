44d3859b5eece67c17e8c3356485402678d0ad9e
kmsayem12 <sayem312518@gmail.com>
2025-05-10 16:48:08 +0600

    initDB in App component

--- App.tsx
+++ App.tsx
@@ -5,13 +5,13 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {KeyboardProvider} from 'react-native-keyboard-controller';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
+import {initDB} from './src/config/sqlite';
 import ChatScreen from './src/screens/ChatScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
@@ -20,53 +20,55 @@ import {RootStackParamList} from './src/types/navigation';
 import RegisterScreen from './src/screens/RegisterScreen';
 import ChatListScreen from './src/screens/ChatListScreen';
 
-// Screens
-
 const Stack = createStackNavigator<RootStackParamList>();
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    initDB()
+      .then(() => console.log('DB initialized'))
+      .catch(err => console.error('Failed to init DB', err));
+  }, []);
+
   return (
     <GestureHandlerRootView style={{flex: 1}}>
-      <KeyboardProvider>
-        <SafeAreaProvider>
-          <NavigationContainer>
-            <Stack.Navigator initialRouteName="Splash">
-              <Stack.Screen
-                name="Splash"
-                component={SplashScreen}
-                options={{headerShown: false}}
-              />
-              <Stack.Screen
-                name="Login"
-                component={LoginScreen}
-                options={{headerShown: false}}
-              />
-              <Stack.Screen
-                name="Register"
-                component={RegisterScreen}
-                // options={{headerShown: false}}
-              />
-              <Stack.Screen
-                name="Home"
-                component={HomeScreen}
-                options={{headerLeft: () => null}}
-              />
-              <Stack.Screen
-                name="ChatList"
-                component={ChatListScreen}
-                options={{title: 'Chats'}}
-              />
-              <Stack.Screen
-                name="Chat"
-                component={ChatScreen}
-                options={({route}) => ({
-                  title: route.params?.userName || 'Chat',
-                })}
-              />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </SafeAreaProvider>
-      </KeyboardProvider>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Splash">
+            <Stack.Screen
+              name="Splash"
+              component={SplashScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Register"
+              component={RegisterScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{headerLeft: () => null}}
+            />
+            <Stack.Screen
+              name="ChatList"
+              component={ChatListScreen}
+              options={{title: 'Chats'}}
+            />
+            <Stack.Screen
+              name="Chat"
+              component={ChatScreen}
+              options={({route}) => ({
+                title: route.params?.userName || 'Chat',
+              })}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
